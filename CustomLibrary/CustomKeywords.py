686536b7f7ed22f1244cbf87ed75e4ec36507a2c
Dhiraj-Gawali <dhirajgawali22@gmail.com>
2024-10-02 21:50:36 +0530

    Code Refactoring

--- CustomLibrary/CustomKeywords.py
+++ CustomLibrary/CustomKeywords.py
@@ -1,9 +1,11 @@
 import random
 import string
 
+from robot.api.deco import keyword, library
 from robot.libraries.BuiltIn import BuiltIn
 
 
+@library
 class CustomKeywords:
     user_prefix = "user_"
     email_extension = "@yopmail.com"
@@ -15,10 +17,12 @@ class CustomKeywords:
     def __init__(self):
         self.selLib = BuiltIn().get_library_instance("SeleniumLibrary")
 
+    @keyword
     def tap_on_register_button(self):
         self.selLib.wait_until_element_is_visible("class:ico-register")
         self.selLib.click_element("class:ico-register")
 
+    @keyword
     def fill_the_login_form_with_wrong_creds(self):
         self.selLib.wait_until_element_is_visible("id:gender-male")
         self.selLib.click_element("id:gender-male")
@@ -29,6 +33,7 @@ class CustomKeywords:
         self.selLib.input_password("id:ConfirmPassword", "flnasdaame@45")
         self.selLib.click_element("xpath://*[@value=\"Register\"]")
 
+    @keyword
     def fill_the_registration_form_with_valid_creds_using_customKeywords(self):
         self.generate_random_user()
         self.selLib.wait_until_element_is_visible("id:gender-male")
@@ -40,12 +45,14 @@ class CustomKeywords:
         self.selLib.input_password("id:ConfirmPassword", self.password)
         self.selLib.click_element("xpath://*[@value=\"Register\"]")
 
+    @keyword
     def generate_random_user(self):
         self.lastname = self.user_prefix + ''.join(random.choices(string.ascii_letters + string.digits, k=5))
         self.firstname = self.user_prefix + ''.join(random.choices(string.ascii_letters + string.digits, k=5))
         self.user_email = self.lastname + self.firstname + self.email_extension
         self.password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
 
+    @keyword
     def add_product_to_cart(self, product_to_add):
         products_ele_list = self.selLib.get_webelements("css:.product-title a")
         i = 1
